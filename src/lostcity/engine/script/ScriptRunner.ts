3cae1a0d0b62f04fdf872d195d954deb173aed44
Joshua Filby <joshua@filby.me>
2023-07-11 01:37:02 -0500

    refactor: extract LocConfigOps.ts

--- src/lostcity/engine/script/ScriptRunner.ts
+++ src/lostcity/engine/script/ScriptRunner.ts
@@ -24,6 +24,7 @@ import NpcOps from "#lostcity/engine/script/handlers/NpcOps.js";
 import LocOps from "#lostcity/engine/script/handlers/LocOps.js";
 import ObjOps from "#lostcity/engine/script/handlers/ObjOps.js";
 import NpcConfigOps from "#lostcity/engine/script/handlers/NpcConfigOps.js";
+import LocConfigOps from "#lostcity/engine/script/handlers/LocConfigOps.js";
 
 export type CommandHandler = (state: ScriptState) => void;
 export type CommandHandlers = {
@@ -41,6 +42,7 @@ export default class ScriptRunner {
         ...LocOps,
         ...ObjOps,
         ...NpcConfigOps,
+        ...LocConfigOps,
 
         [ScriptOpcode.ERROR]: (state) => {
             throw new Error(state.popString());
