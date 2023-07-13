b61f91419055b4274b547e02666995bebef2f35f
James Monger <jameskmonger@hotmail.co.uk>
2023-07-13 02:21:19 +0100

    refactor: convert most /rsmod/ code to TypeScript (#27)
    * refactor: convert rsmod code to TypeScript
    
    * refactor: qualify statics with LineOfSight.

--- src/rsmod/reach/ReachStrategy.ts
+++ src/rsmod/reach/ReachStrategy.ts
@@ -1,3 +1,4 @@
+import CollisionFlagMap from '#rsmod/collision/CollisionFlagMap.js';
 import RotationUtils from '../utils/RotationUtils.js';
 import RectangleBoundaryUtils from './RectangleBoundaryUtils.js';
 
@@ -8,7 +9,7 @@ export default class ReachStrategy {
     static NO_STRATEGY = 3;
     static RECTANGLE_EXCLUSIVE_STRATEGY = 4;
 
-    static exitStrategy(locShape) {
+    static exitStrategy(locShape: number) {
         if (locShape == -2) {
             return ReachStrategy.RECTANGLE_EXCLUSIVE_STRATEGY;
         } else if (locShape == -1) {
@@ -24,11 +25,11 @@ export default class ReachStrategy {
         }
     }
 
-    static alteredRotation(rotation, shape) {
+    static alteredRotation(rotation: number, shape: number) {
         return shape == 7 ? ((rotation + 2) & 0x3) : rotation;
     }
 
-    static reached(flags, level, srcX, srcZ, destX, destZ, destWidth, destHeight, srcSize, locRot = 0, locShape = -1, blockAccessFlags = 0) {
+    static reached(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, destWidth: number, destHeight: number, srcSize: number, locRot = 0, locShape = -1, blockAccessFlags = 0) {
         let exitStrategy = ReachStrategy.exitStrategy(locShape);
         if (exitStrategy != ReachStrategy.RECTANGLE_EXCLUSIVE_STRATEGY && srcX == destX && srcZ == destZ) {
             return true;
@@ -47,7 +48,7 @@ export default class ReachStrategy {
         }
     }
 
-    static reachRectangle(flags, level, srcX, srcZ, destX, destZ, srcSize, destWidth, destHeight, locRot, blockAccessFlags) {
+    static reachRectangle(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, destWidth: number, destHeight: number, locRot: number, blockAccessFlags: number) {
         let rotatedWidth = RotationUtils.rotate(locRot, destWidth, destHeight);
         let rotatedHeight = RotationUtils.rotate(locRot, destHeight, destWidth);
         let rotatedBlockAccess = RotationUtils.rotateFlags(locRot, blockAccessFlags);
@@ -61,7 +62,7 @@ export default class ReachStrategy {
         }
     }
 
-    static reachExclusiveRectangle(flags, level, srcX, srcZ, destX, destZ, srcSize, destWidth, destHeight, locRot, blockAccessFlags) {
+    static reachExclusiveRectangle(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, destWidth: number, destHeight: number, locRot: number, blockAccessFlags: number) {
         let rotatedWidth = RotationUtils.rotate(locRot, destWidth, destHeight);
         let rotatedHeight = RotationUtils.rotate(locRot, destHeight, destWidth);
         let rotatedBlockAccess = RotationUtils.rotateFlags(locRot, blockAccessFlags);
@@ -75,7 +76,7 @@ export default class ReachStrategy {
         }
     }
 
-    static reachWall(flags, level, srcX, srcZ, destX, destZ, srcSize, locShape, locRot) {
+    static reachWall(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, locShape: number, locRot: number) {
         if (srcSize == 1 && srcX == destX && srcZ == destZ) {
             return true;
         } else if (srcSize != 1 && destX >= srcX && srcSize + srcX - 1 >= destX && destZ >= srcZ && srcSize + srcZ - 1 >= destZ) {
@@ -87,7 +88,7 @@ export default class ReachStrategy {
         }
     }
 
-    static reachWallDeco(flags, level, srcX, srcZ, destX, destZ, srcSize, locShape, locRot) {
+    static reachWallDeco(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, locShape: number, locRot: number) {
         if (srcSize == 1 && srcX == destX && srcZ == destZ) {
             return true;
         } else if (srcSize != 1 && destX >= srcX && srcSize + srcX - 1 >= destX && destZ >= srcZ && srcSize + srcZ - 1 >= destZ) {
@@ -101,15 +102,15 @@ export default class ReachStrategy {
 
     // ----
 
-    static reachWall1(flags, level, srcX, srcZ, destX, destZ, locShape, locRot) {
+    static reachWall1(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, locShape: number, locRot: number) {
     }
 
-    static reachWallN(flags, level, srcX, srcZ, destX, destZ, srcSize, locShape, locRot) {
+    static reachWallN(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, locShape: number, locRot: number) {
     }
 
-    static reachWallDeco1(flags, level, srcX, srcZ, destX, destZ, locShape, locRot) {
+    static reachWallDeco1(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, locShape: number, locRot: number) {
     }
 
-    static reachWallDecoN(flags, level, srcX, srcZ, destX, destZ, srcSize, locShape, locRot) {
+    static reachWallDecoN(flags: CollisionFlagMap, level: number, srcX: number, srcZ: number, destX: number, destZ: number, srcSize: number, locShape: number, locRot: number) {
     }
 }
