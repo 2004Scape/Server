b39622957ecee307121d459cb381a7470f0f70f2
James Monger <jameskmonger@hotmail.co.uk>
2023-07-21 13:15:49 +0100

    test: add tests to ReachStrategy class
    port `ReachStrategyTest` from Kotlin

---
+++ src/rsmod/reach/ReachStrategyTest.test.ts
@@ -0,0 +1,197 @@
+import CollisionFlagMap from "#rsmod/collision/CollisionFlagMap";
+import CollisionFlag from "#rsmod/flag/CollisionFlag";
+import ReachStrategy from "./ReachStrategy";
+
+describe('ReachStrategyTest', () => {
+    /**
+     * Test that object rotations are taken into account within [ReachStrategy.reached]
+     * and do not rely on external modifications. For example, given the parameters of
+     * an object in coordinates (3203, 3203) with a dimension of 3 x 1 (width x height),
+     * the following test should pass:
+     *
+     * Object rotation of [0] or [2]. (normal)
+     * ```
+     * --------
+     * --------
+     * --------
+     * ---ooo--
+     * --o   o-
+     * ---ooo--
+     * --------
+     * --------
+     * ```
+     * Where:
+     * - Area starts from bottom-left and makes its way to top-right. (3200,3200 - 3207,3207)
+     * - ' ' (whitespace) are the tiles occupied by the rotated object.
+     * - 'o' are the tiles that successfully pass [ReachStrategy.reached].
+     * - '-' represents every other tile in the area. (in this case a zone, or 8x8 tile area)
+     */
+    test.each([
+        [ 3203, 3203, { width: 1, height: 1 } ],
+        [ 3203, 3203, { width: 1, height: 2 } ],
+        [ 3203, 3203, { width: 1, height: 3 } ],
+        [ 3203, 3203, { width: 2, height: 1 } ],
+        [ 3203, 3203, { width: 2, height: 2 } ],
+        [ 3203, 3203, { width: 2, height: 3 } ],
+        [ 3203, 3203, { width: 3, height: 1 } ],
+        [ 3203, 3203, { width: 3, height: 2 } ],
+        [ 3203, 3203, { width: 3, height: 3 } ],
+    ])('test rotated object normal ', (objX, objZ, { width, height }) => {
+        const LEVEL = 0;
+
+        const minX = objX - 16, minZ = objZ - 16;
+        const maxX = objX + 16, maxZ = objZ + 16;
+
+        const map = new CollisionFlagMap();
+        
+        // (jkm) our implementation (unlike Kotlin) doesn't allow setting some width/height square, so we have to do it manually
+        for (let _z = 0; _z < height; _z++) {
+            for (let _x = 0; _x < width; _x++) {
+                map.add(objX + _x, objZ + _z, LEVEL, CollisionFlag.LOC);
+            }
+        }
+
+        const reached = (srcX: number, srcZ: number, rot: number, blockAccessFlags: number = 0) => {
+            return ReachStrategy.reached(
+                map,
+                LEVEL,
+                srcX,
+                srcZ,
+                objX,
+                objZ,
+                width,
+                height,
+                1,
+                rot,
+                -2, // Use rectangular excludive strategy
+                blockAccessFlags
+            )
+        };
+        
+        for (let x = 0; x < width; x++) {
+            // Test coming from south tiles.
+            expect(reached(objX + x, objZ - 1, 0)).toBeTruthy();
+            expect(reached(objX + x, objZ - 1, 2)).toBeTruthy();
+            // Test coming from north tiles.
+            expect(reached(objX + x, objZ + height, 0)).toBeTruthy();
+            expect(reached(objX + x, objZ + height, 2)).toBeTruthy();
+            // Test coming from south tiles with access blocked.
+            expect(reached(objX + x, objZ - 1, 0, CollisionFlag.BLOCK_SOUTH)).toBeFalsy();
+            expect(reached(objX + x, objZ - 1, 2, CollisionFlag.BLOCK_NORTH)).toBeFalsy();
+            // Test coming from north tiles with access blocked.
+            expect(reached(objX + x, objZ + height, 0, CollisionFlag.BLOCK_NORTH)).toBeFalsy();
+            expect(reached(objX + x, objZ + height, 2, CollisionFlag.BLOCK_SOUTH)).toBeFalsy();
+        }
+        
+        for (let z = 0; z < height; z++) {
+            // Test coming from west tiles.
+            expect(reached(objX - 1, objZ + z, 0)).toBeTruthy();
+            expect(reached(objX - 1, objZ + z, 2)).toBeTruthy();
+            // Test coming from east tiles.
+            expect(reached(objX + width, objZ + z, 0)).toBeTruthy();
+            expect(reached(objX + width, objZ + z, 2)).toBeTruthy();
+            // Test coming from west tiles with access blocked.
+            expect(reached(objX - 1, objZ + z, 0, CollisionFlag.BLOCK_WEST)).toBeFalsy();
+            expect(reached(objX - 1, objZ + z, 2, CollisionFlag.BLOCK_EAST)).toBeFalsy();
+            // Test coming from east tiles with access blocked.
+            expect(reached(objX + width, objZ + z, 0, CollisionFlag.BLOCK_EAST)).toBeFalsy();
+            expect(reached(objX + width, objZ + z, 2, CollisionFlag.BLOCK_WEST)).toBeFalsy();
+        }        
+    });
+
+    /**
+     * Test that object rotations are taken into account within [ReachStrategy.reached]
+     * and do not rely on external modifications. For example, given the parameters of
+     * an object in coordinates (3203, 3203) with a dimension of 3 x 1 (width x height),
+     * the following test should pass:
+     *
+     * Object rotation of [1] or [3]. (flipped)
+     * ```
+     * --------
+     * ---o----
+     * --o o---
+     * --o o---
+     * --o o---
+     * ---o----
+     * --------
+     * --------
+     * ```
+     * Where:
+     * - Area starts from bottom-left and makes its way to top-right. (3200,3200 - 3207,3207)
+     * - ' ' (whitespace) are the tiles occupied by the rotated object.
+     * - 'o' are the tiles that successfully pass [ReachStrategy.reached].
+     * - '-' represents every other tile in the area. (in this case a zone, or 8x8 tile area)
+     */
+    test.each([
+        [ 3203, 3203, { width: 1, height: 1 } ],
+        [ 3203, 3203, { width: 1, height: 2 } ],
+        [ 3203, 3203, { width: 1, height: 3 } ],
+        [ 3203, 3203, { width: 2, height: 1 } ],
+        [ 3203, 3203, { width: 2, height: 2 } ],
+        [ 3203, 3203, { width: 2, height: 3 } ],
+        [ 3203, 3203, { width: 3, height: 1 } ],
+        [ 3203, 3203, { width: 3, height: 2 } ],
+        [ 3203, 3203, { width: 3, height: 3 } ],
+    ])('test rotated object flipped', (objX, objZ, { width, height }) => {
+        const LEVEL = 0;
+    
+        const minX = objX - 16, minZ = objZ - 16;
+        const maxX = objX + 16, maxZ = objZ + 16;
+    
+        const map = new CollisionFlagMap();
+            
+        // (jkm) our implementation (unlike Kotlin) doesn't allow setting some width/height square, so we have to do it manually
+        for (let _z = 0; _z < width; _z++) {  // width and height are swapped
+            for (let _x = 0; _x < height; _x++) {  // width and height are swapped
+                map.add(objX + _x, objZ + _z, LEVEL, CollisionFlag.LOC);
+            }
+        }
+    
+        const reached = (srcX: number, srcZ: number, rot: number, blockAccessFlags: number = 0) => {
+            return ReachStrategy.reached(
+                map,
+                LEVEL,
+                srcX,
+                srcZ,
+                objX,
+                objZ,
+                width,
+                height,
+                1,
+                rot,
+                -2, // Use rectangular exclusive strategy
+                blockAccessFlags
+            )
+        };
+            
+        for (let x = 0; x < height; x++) {  // width and height are swapped
+            // Test coming from south tiles.
+            expect(reached(objX + x, objZ - 1, 1)).toBeTruthy();
+            expect(reached(objX + x, objZ - 1, 3)).toBeTruthy();
+            // Test coming from north tiles.
+            expect(reached(objX + x, objZ + width, 1)).toBeTruthy();  // width and height are swapped
+            expect(reached(objX + x, objZ + width, 3)).toBeTruthy();  // width and height are swapped
+            // Test coming from south tiles with access blocked.
+            expect(reached(objX + x, objZ - 1, 1, CollisionFlag.BLOCK_EAST)).toBeFalsy();
+            expect(reached(objX + x, objZ - 1, 3, CollisionFlag.BLOCK_WEST)).toBeFalsy();
+            // Test coming from north tiles with access blocked.
+            expect(reached(objX + x, objZ + width, 1, CollisionFlag.BLOCK_WEST)).toBeFalsy();  // width and height are swapped
+            expect(reached(objX + x, objZ + width, 3, CollisionFlag.BLOCK_EAST)).toBeFalsy();  // width and height are swapped
+        }
+            
+        for (let z = 0; z < width; z++) {  // width and height are swapped
+            // Test coming from west tiles.
+            expect(reached(objX - 1, objZ + z, 1)).toBeTruthy();
+            expect(reached(objX - 1, objZ + z, 3)).toBeTruthy();
+            // Test coming from east tiles.
+            expect(reached(objX + height, objZ + z, 1)).toBeTruthy();  // width and height are swapped
+            expect(reached(objX + height, objZ + z, 3)).toBeTruthy();  // width and height are swapped
+            // Test coming from west tiles with access blocked.
+            expect(reached(objX - 1, objZ + z, 1, CollisionFlag.BLOCK_SOUTH)).toBeFalsy();
+            expect(reached(objX - 1, objZ + z, 3, CollisionFlag.BLOCK_NORTH)).toBeFalsy();
+            // Test coming from east tiles with access blocked.
+            expect(reached(objX + height, objZ + z, 1, CollisionFlag.BLOCK_NORTH)).toBeFalsy();  // width and height are swapped
+            expect(reached(objX + height, objZ + z, 3, CollisionFlag.BLOCK_SOUTH)).toBeFalsy();  // width and height are swapped
+        }        
+    });
+});
