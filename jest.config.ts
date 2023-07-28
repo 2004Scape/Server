45ce2f6ea4ab6279e866c55c663d7bb953aa4b01
James Monger <jameskmonger@hotmail.co.uk>
2023-07-28 10:56:15 +0100

    test: set up coverage report properly

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,13 @@ const config: Config = {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        'src/**/*.js',
+        '!src/**/*.test.ts',
+        '!src/**/*.d.ts',
+        '!src/lostcity/web/**/*'
+    ],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
