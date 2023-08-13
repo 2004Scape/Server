ea13bccccd52f408600e1fa775488825c945d2eb
Pazaz <pazaz@itspazaz.com>
2023-08-13 03:49:55 -0400

    fix: Standalone webserver changes

--- src/lostcity/cache/CrcTable.js
+++ src/lostcity/cache/CrcTable.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import Packet from '#jagex2/io/Packet.js';
 
 export const CrcBuffer = new Packet();
@@ -9,14 +10,16 @@ function makeCrc(path) {
     CrcBuffer.p4(crc);
 }
 
-CrcBuffer.p4(0);
-makeCrc('data/pack/client/title');
-makeCrc('data/pack/client/config');
-makeCrc('data/pack/client/interface');
-makeCrc('data/pack/client/media');
-makeCrc('data/pack/client/models');
-makeCrc('data/pack/client/textures');
-makeCrc('data/pack/client/wordenc');
-makeCrc('data/pack/client/sounds');
+if (fs.existsSync('data/pack/client/')) {
+    CrcBuffer.p4(0);
+    makeCrc('data/pack/client/title');
+    makeCrc('data/pack/client/config');
+    makeCrc('data/pack/client/interface');
+    makeCrc('data/pack/client/media');
+    makeCrc('data/pack/client/models');
+    makeCrc('data/pack/client/textures');
+    makeCrc('data/pack/client/wordenc');
+    makeCrc('data/pack/client/sounds');
+}
 
 export const CrcBuffer32 = Packet.crc32(CrcBuffer.data);
