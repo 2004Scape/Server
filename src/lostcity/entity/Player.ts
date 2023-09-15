5195e1e5df7eb19a94ed1419c98b61747bd89450
Jordan <personal.jordanabraham@gmail.com>
2023-09-15 14:50:35 -0400

    feat: Implement p_countdialog Command (#145)
    * Implement the p_countdialog command.
    
    * Update scripts.md
    
    * Fix typo in scripts.md

--- src/lostcity/entity/Player.ts
+++ src/lostcity/entity/Player.ts
@@ -455,7 +455,7 @@ export default class Player extends PathingEntity {
 
     activeScript: ScriptState | null = null;
     resumeButtons: number[] = [];
-    lastInt = 0; // p_countdialog input
+    lastInt: number | null = null; // p_countdialog input
     lastItem: number | null = null;
     lastVerifyObj: number | null = null;
     lastSlot: number | null = null;
@@ -606,9 +606,7 @@ export default class Player extends PathingEntity {
                     this.executeScript(this.activeScript);
                 }
             } else if (opcode === ClientProt.RESUME_P_COUNTDIALOG) {
-                const count = data.g4();
-
-                this.lastInt = count;
+                this.lastInt = data.g4();
                 if (this.activeScript) {
                     this.executeScript(this.activeScript);
                 }
