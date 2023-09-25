78c316c7d1655af2d729a272519630ec2bb53720
Pazaz <pazaz@itspazaz.com>
2023-09-25 13:46:06 -0400

    fix: Linter config errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,10 +35,6 @@ module.exports = {
             4,
             { 'SwitchCase': 1 }
         ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
         'quotes': [
             'error',
             'single'
