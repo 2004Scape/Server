a6db38be3eea281e892a03409bd35effe416a3f5
Jordan <personal.jordanabraham@gmail.com>
2023-10-04 21:51:50 -0400

    Add fixes to npc wanderrange and attackrange properties.

--- src/lostcity/entity/Player.ts
+++ src/lostcity/entity/Player.ts
@@ -1350,7 +1350,7 @@ export default class Player extends PathingEntity {
                     locType.width,
                     locType.length,
                     locType.id,
-                    10,
+                    0,
                     0
                 );
                 World.addLoc(entity, 500);
