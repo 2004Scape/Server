565a1f45e75fa1c8237a8ba0dcc7fab1497e35d5
Indio <indioalt@gmail.com>
2023-12-10 21:06:14 -0500

    change npc_walk to directly queue to dest x/z rather than pathfind there

--- src/lostcity/engine/script/handlers/NpcOps.ts
+++ src/lostcity/engine/script/handlers/NpcOps.ts
@@ -353,7 +353,7 @@ const NpcOps: CommandHandlers = {
         const pos = Position.unpackCoord(coord);
         const npc = state.activeNpc;
 
-        npc.queueWaypoints(World.pathFinder.findPath(npc.level, npc.x, npc.z, pos.x, pos.z, npc.width, npc.width, npc.length, npc.orientation).waypoints);
+        npc.queueWaypoint(pos.x, pos.z);
     }),
 
     [ScriptOpcode.NPC_CHANGETYPE]: checkedHandler(ActiveNpc, (state) => {
