b2153dc8ecb308b835ab3de1ec922524733cb179
Pazaz <pazaz@itspazaz.com>
2024-01-02 09:35:26 -0500

    feat: jav_config and serverlist routes

--- src/lostcity/web/routes/client.ts
+++ src/lostcity/web/routes/client.ts
@@ -1,6 +1,102 @@
 import WorldList from '#lostcity/engine/WorldList.js';
 
+import Environment from '#lostcity/util/Environment.js';
+
 export default function (f: any, opts: any, next: any) {
+    f.get('/jav_config.ws', async (req: any, res: any) => {
+        let address = (Environment.HTTPS_ENABLED ? 'https://' : 'http://') + Environment.PUBLIC_IP;
+        if (Environment.ADDRESS_SHOWPORT) {
+            if (Environment.HTTPS_ENABLED && (Environment.WEB_PORT != 443 && Environment.WEB_PORT != 80)) {
+                address += ':' + Environment.WEB_PORT;
+            } else if (!Environment.HTTPS_ENABLED && Environment.WEB_PORT != 80) {
+                address += ':' + Environment.WEB_PORT;
+            }
+        }
+
+        const config: string[] = [
+            'viewerversion=100',
+            'title=Lost City',
+            `serverlist=${address}/serverlist.ws`,
+            'loader_jar=loader.jar',
+            'window_preferredwidth=774',
+            'window_preferredheight=552',
+            'applet_minwidth=789',
+            'applet_minheight=532',
+            'applet_maxwidth=5670',
+            'applet_maxheight=2160',
+        ];
+
+        const localization: string[] = [
+            'lang0=English',
+            'ok=OK',
+            'cancel=Cancel',
+            'options=Options',
+            'changes_on_restart=Your changes will take effect when you next start this program.',
+            'copy_paste_url=Please copy and paste the following URL into your web browser',
+            'err_create_advertising=Unable to create advertising',
+            'err_create_target=Unable to create target applet',
+            'err_downloading=Error downloading',
+            'err_get_file=Error getting file',
+            'err_save_file=Error saving file',
+            'err_load_bc=Unable to load browsercontrol',
+            'err_verify_bc=Unable to verify browsercontrol',
+            'err_verify_bc64=Unable to verify browsercontrol64',
+            'information=Information',
+            'language=Language',
+            'switchserver=Switch World',
+            'loading_app=Loading application',
+            'loading_app_resources=Loading application resources',
+            'message=Message',
+            'new_version=Update available! Please visit the website to download the latest version.',
+            'new_version_link=http://2004scape.org/',
+            'new_version_linktext=Open Homepage',
+            'tandc=This game is copyright (c) 1999 - 2004 Jagex Ltd.\Recreated and preserved in 2023.'
+        ];
+        config.push(...localization.map(x => 'msg=' + x));
+
+        const servers: string[][] = [];
+        for (const world of WorldList) {
+            // legacy PCs are more likely to have issues with HTTPS
+            const address = world.address.replace('https:', 'http:');
+
+            servers.push([
+                `[${world.id}.high]`,
+                `title=2004scape - World ${world.id} (${world.members ? 'Members' : 'Free'})`,
+                `codebase=${address}`,
+                `servername=World ${world.id} (${world.members ? 'Members' : 'Free'}), High Detail`,
+                `param=nodeid=${10 + world.id - 1}`,
+                `param=portoff=${world.portOffset}`,
+                `param=free=${world.members ? 0 : 1}`,
+                'param=lowmem=0'
+            ]);
+
+            servers.push([
+                `[${world.id}.low]`,
+                `title=2004scape - World ${world.id} (${world.members ? 'Members' : 'Free'})`,
+                `codebase=${address}`,
+                `servername=World ${world.id} (${world.members ? 'Members' : 'Free'}), Low Detail`,
+                `param=nodeid=${10 + world.id - 1}`,
+                `param=portoff=${world.portOffset}`,
+                `param=free=${world.members ? 0 : 1}`,
+                'param=lowmem=1'
+            ]);
+        }
+        config.push(...servers.flat());
+
+        return config.join('\n');
+    });
+
+    f.get('/serverlist.ws', async (req: any, res: any) => {
+        const list: string[] = [];
+
+        for (const world of WorldList) {
+            list.push(`[${world.id}.high]`);
+            list.push(`[${world.id}.low]`);
+        }
+
+        return list.join('\n');
+    });
+
     f.get('/banner', async (req: any, res: any) => {
         return res.view('banner');
     });
@@ -30,13 +126,13 @@ export default function (f: any, opts: any, next: any) {
                 method: req.query.method,
             });
         } else if (req.query.method == 1) {
-            return res.redirect('/client/rs2-client.jar');
-        } else if (req.query.method == 2) {
             return res.view('javaclient', {
                 world,
                 detail: req.query.detail,
                 method: req.query.method,
             });
+        }  else {
+            return res.redirect(302, '/');
         }
     });
 
@@ -64,12 +160,14 @@ export default function (f: any, opts: any, next: any) {
                 detail: req.query.detail,
                 method: req.query.method,
             });
-        } else if (req.query.method == 2) {
+        } else if (req.query.method == 1) {
             return res.view('javaclient-inner', {
                 world,
                 detail: req.query.detail,
                 method: req.query.method,
             });
+        } else {
+            return res.redirect(302, '/');
         }
     });
 
