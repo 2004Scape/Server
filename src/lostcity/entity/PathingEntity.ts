aa5ce851cfd8e7290ca82f729ccff718580522ee
Indio <indioalt@gmail.com>
2024-02-14 10:10:11 -0500

    change round to bitwise OR for the p_teleport fix

--- src/lostcity/entity/PathingEntity.ts
+++ src/lostcity/entity/PathingEntity.ts
@@ -266,8 +266,8 @@ export default abstract class PathingEntity extends Entity {
         if (tele && !this.jump && distanceMoved <= 2) {
             if (distanceMoved === 2) {
                 // run
-                const firstX = Math.round((this.x + this.lastX) / 2);
-                const firstZ = Math.round((this.z + this.lastZ) / 2);
+                const firstX = ((this.x + this.lastX) / 2) | 0;
+                const firstZ = ((this.z + this.lastZ) / 2) | 0;
                 walkDir = Position.face(this.lastX, this.lastZ, firstX, firstZ);
                 runDir = Position.face(firstX, firstZ, this.x, this.z);
             } else {
