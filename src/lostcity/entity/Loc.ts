247cf024a87b932202be10aa16fd83e9e809bd17
Jordan <personal.jordanabraham@gmail.com>
2024-04-23 21:07:06 -0400

    feat: bitpack loc type, shape and angle

--- src/lostcity/entity/Loc.ts
+++ src/lostcity/entity/Loc.ts
@@ -2,14 +2,23 @@ import NonPathingEntity from '#lostcity/entity/NonPathingEntity.js';
 
 export default class Loc extends NonPathingEntity {
     // constructor properties
-    type: number;
-    shape: number;
-    angle: number;
+    private readonly info: number;
 
     constructor(level: number, x: number, z: number, width: number, length: number, type: number, shape: number, angle: number) {
         super(level, x, z, width, length);
-        this.type = type;
-        this.shape = shape;
-        this.angle = angle;
+        // 16383, 31, 3
+        this.info = (type & 0x3fff) | (shape & 0x1f) << 14 | (angle & 0x3) << 19;
+    }
+
+    get type(): number {
+        return this.info & 0x3fff;
+    }
+
+    get shape(): number {
+        return (this.info >> 14) & 0x1f;
+    }
+
+    get angle(): number {
+        return (this.info >> 19) & 0x3;
     }
 }
