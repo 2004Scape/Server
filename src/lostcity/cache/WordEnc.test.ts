935891dd99d90a64980213a387fe3288c2190d34
James Monger <jameskmonger@hotmail.co.uk>
2024-04-30 21:38:33 +0100

    tests: fix Packet2 mock (bad merge)

--- src/lostcity/cache/WordEnc.test.ts
+++ src/lostcity/cache/WordEnc.test.ts
@@ -1,4 +1,4 @@
-import Packet from '#jagex2/io/Packet.js';
+import Packet2 from '#jagex2/io/Packet2.js';
 
 import fs from 'fs';
 
@@ -17,18 +17,18 @@ describe('WordEnc', () => {
             const dat = new Packet();
 
             fs.existsSync = vi.fn().mockReturnValue(true);
-            Packet.load = vi.fn().mockReturnValue(dat);
+            Packet2.load = vi.fn().mockReturnValue(dat);
 
             WordEnc.load('/path/to/data');
 
-            expect(Packet.load).toHaveBeenCalledWith('/path/to/data/client/wordenc');
+            expect(Packet2.load).toHaveBeenCalledWith('/path/to/data/client/wordenc');
         });
 
         it('should return early if wordenc does not exist', () => {
             fs.existsSync = vi.fn().mockReturnValue(false);
-            Packet.load = vi.fn().mockReturnValue(undefined);
+            Packet2.load = vi.fn().mockReturnValue(undefined);
 
-            expect(Packet.load).not.toHaveBeenCalled();
+            expect(Packet2.load).not.toHaveBeenCalled();
         });
     });
 
