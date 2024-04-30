2fe51ed4347d9501bff355a8bf2f5852596b567b
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2024-04-29 21:42:18 -0400

    chore: add comment on interface CRC check

--- src/lostcity/tools/packinterface/PackClient.ts
+++ src/lostcity/tools/packinterface/PackClient.ts
@@ -12,6 +12,10 @@ export function packClientInterface() {
     const jag = new Jagfile();
     const data = packInterface(false);
 
+    /**
+     * TODO: Enable this once we are feature complete.
+     * Some interfaces are edited so the CRC check will fail (i.e. unfinished quests in the quest tab).
+     */
     // if (!Environment.CI_MODE && !Packet.checkcrc(data, -2146838800)) {
     //     console.error('.if CRC check failed! Custom data detected.');
     //     process.exit(1);
