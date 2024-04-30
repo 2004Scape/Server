a7421bd9c72fa763f81637e67ba274ebd93595e4
Jordan <personal.jordanabraham@gmail.com>
2024-04-30 07:51:59 -0400

    feat: staffmodlevel 3 support

--- src/lostcity/entity/PlayerLoading.ts
+++ src/lostcity/entity/PlayerLoading.ts
@@ -128,7 +128,9 @@ export class PlayerLoading {
         player.combatLevel = player.getCombatLevel();
         player.lastResponse = World.currentTick;
 
-        if (Environment.LOCAL_DEV || Environment.JMODS.find(name => name === safeName) !== undefined) {
+        if (Environment.LOCAL_DEV) {
+            player.staffModLevel = 3;
+        } else if (Environment.JMODS.find(name => name === safeName) !== undefined) {
             player.staffModLevel = 2;
         }
         return player;
