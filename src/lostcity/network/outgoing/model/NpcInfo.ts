179a65c8b18215e64893600b0a297c19c9c866f8
Jordan <personal.jordanabraham@gmail.com>
2024-06-26 09:54:45 -0400

    chore: update NpcInfo and PlayerInfo messages

--- src/lostcity/network/outgoing/model/NpcInfo.ts
+++ src/lostcity/network/outgoing/model/NpcInfo.ts
@@ -2,6 +2,7 @@ import OutgoingMessage from '#lostcity/network/outgoing/OutgoingMessage.js';
 import ServerProtPriority from '#lostcity/network/outgoing/prot/ServerProtPriority.js';
 import Player from '#lostcity/entity/Player.js';
 
+// this is slightly unsafe.
 export default class NpcInfo extends OutgoingMessage {
     priority = ServerProtPriority.HIGH;
 
