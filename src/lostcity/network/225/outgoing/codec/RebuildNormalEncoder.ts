ae9706bc7378dabf912d3a912419238049514266
Jordan <personal.jordanabraham@gmail.com>
2024-06-26 10:29:16 -0400

    feat: simplify RebuildNormal

--- src/lostcity/network/225/outgoing/codec/RebuildNormalEncoder.ts
+++ src/lostcity/network/225/outgoing/codec/RebuildNormalEncoder.ts
@@ -2,41 +2,21 @@ import MessageEncoder from '#lostcity/network/outgoing/codec/MessageEncoder.js';
 import Packet from '#jagex2/io/Packet.js';
 import ServerProt from '#lostcity/network/225/outgoing/prot/ServerProt.js';
 import RebuildNormal from '#lostcity/network/outgoing/model/RebuildNormal.js';
-import {Position} from '#lostcity/entity/Position.js';
 import {PRELOADED_CRC} from '#lostcity/server/PreloadedPacks.js';
-
-type MapSquare = {
-    x: number;
-    z: number;
-}
 
 export default class RebuildNormalEncoder extends MessageEncoder<RebuildNormal> {
     prot = ServerProt.REBUILD_NORMAL;
 
     encode(buf: Packet, message: RebuildNormal): void {
-        const {zoneX, zoneZ} = message;
-
-        buf.p2(zoneX);
-        buf.p2(zoneZ);
-
-        // build area is 13x13 zones (8*13 = 104 tiles), so we need to load 6 zones in each direction
-        const squares: MapSquare[] = [];
-        for (let x: number = zoneX - 6; x <= zoneX + 6; x++) {
-            const mx: number = Position.mapsquare(x << 3);
-            for (let z: number = zoneZ - 6; z <= zoneZ + 6; z++) {
-                const mz: number = Position.mapsquare(z << 3);
-
-                if (squares.findIndex(mapsquare => mapsquare.x === mx && mapsquare.z === mz) === -1) {
-                    squares.push({ x: mx, z: mz });
-                }
-            }
-        }
-
-        for (const mapsquare of squares) {
-            buf.p1(mapsquare.x);
-            buf.p1(mapsquare.z);
-            buf.p4(PRELOADED_CRC.get(`m${mapsquare.x}_${mapsquare.z}`) ?? 0);
-            buf.p4(PRELOADED_CRC.get(`l${mapsquare.x}_${mapsquare.z}`) ?? 0);
+        buf.p2(message.zoneX);
+        buf.p2(message.zoneZ);
+        for (const packed of message.mapsquares) {
+            const x: number = packed >> 8;
+            const z: number = packed & 0xff;
+            buf.p1(x);
+            buf.p1(z);
+            buf.p4(PRELOADED_CRC.get(`m${x}_${z}`) ?? 0);
+            buf.p4(PRELOADED_CRC.get(`l${x}_${z}`) ?? 0);
         }
     }
 }
--- src/lostcity/network/outgoing/model/RebuildNormal.ts
+++ src/lostcity/network/outgoing/model/RebuildNormal.ts
@@ -1,5 +1,6 @@
 import OutgoingMessage from '#lostcity/network/outgoing/OutgoingMessage.js';
 import ServerProtPriority from '#lostcity/network/outgoing/prot/ServerProtPriority.js';
+import {Position} from '#lostcity/entity/Position.js';
 
 export default class RebuildNormal extends OutgoingMessage {
     priority = ServerProtPriority.HIGH;
@@ -9,5 +10,22 @@ export default class RebuildNormal extends OutgoingMessage {
         readonly zoneZ: number
     ) {
         super();
+    }
+
+    get mapsquares(): Set<number> {
+        const minX: number = this.zoneX - 6;
+        const maxX: number = this.zoneX + 6;
+        const minZ: number = this.zoneZ - 6;
+        const maxZ: number = this.zoneZ + 6;
+        const result: Set<number> = new Set();
+        // build area is 13x13 zones (8*13 = 104 tiles), so we need to load 6 zones in each direction
+        for (let x: number = minX; x <= maxX; x++) {
+            const mx: number = Position.mapsquare(x << 3);
+            for (let z: number = minZ; z <= maxZ; z++) {
+                const mz: number = Position.mapsquare(z << 3);
+                result.add(mx << 8 | mz);
+            }
+        }
+        return result;
     }
 }
