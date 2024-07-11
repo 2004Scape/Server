5e05b9f669424e6910146a0737125421ffbd1edc
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-07-11 06:26:21 +0200

    fix(engine): missing file check (#652)

--- src/lostcity/cache/config/SeqFrame.ts
+++ src/lostcity/cache/config/SeqFrame.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import Packet from '#jagex2/io/Packet.js';
 
 // partial frame class - only delays, not loading transforms
@@ -6,6 +8,11 @@ export default class SeqFrame {
 
     static load(dir: string) {
         SeqFrame.instances = [];
+
+        if (!fs.existsSync(`${dir}/server/frame_del.dat`)) {
+            console.log('Warning: No frame_del.dat found.');
+            return;
+        }
 
         const frame_del = Packet.load(`${dir}/server/frame_del.dat`);
         for (let i = 0; i < frame_del.data.length; i++) {
