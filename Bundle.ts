23694af3c4269e1b5fcf7456542b4aabc20f126a
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2024-07-13 23:04:58 +0200

    fix: remove dupe environment defines

--- Bundle.ts
+++ Bundle.ts
@@ -1,11 +1,43 @@
 import fs from 'fs';
 
 const modules: string[] = ['module', 'watcher', 'worker_threads', 'dotenv/config', 'bcrypt', '#lostcity/db/query.js', '#lostcity/util/PackFile.js'];
+const defines = {
+    'process.platform': JSON.stringify('webworker'),
+    'process.env.WEB_PORT': '',
+    'process.env.WEB_CORS': '',
+    'process.env.NODE_ID': '',
+    'process.env.NODE_PORT': '',
+    'process.env.NODE_MEMBERS': '',
+    'process.env.NODE_XPRATE': '',
+    'process.env.NODE_PRODUCTION': '',
+    'process.env.NODE_KILLTIMER': '',
+    'process.env.NODE_ALLOW_CHEATS': '',
+    'process.env.NODE_DEBUG': '',
+    'process.env.NODE_DEBUG_PROFILE': '',
+    'process.env.NODE_STAFF': '',
+    'process.env.NODE_CLIENT_ROUTEFINDER': '',
+    'process.env.NODE_SOCKET_TIMEOUT': '',
+    'process.env.LOGIN_HOST': '',
+    'process.env.LOGIN_PORT': '',
+    'process.env.LOGIN_KEY': '',
+    'process.env.DB_HOST': '',
+    'process.env.DB_USER': '',
+    'process.env.DB_PASS': '',
+    'process.env.DB_NAME': '',
+    'process.env.BUILD_JAVA_PATH': '',
+    'process.env.BUILD_STARTUP': '',
+    'process.env.BUILD_STARTUP_UPDATE': '',
+    'process.env.BUILD_VERIFY': '',
+    'process.env.BUILD_VERIFY_FOLDER': '',
+    'process.env.BUILD_VERIFY_PACK': '',
+    'process.env.BUILD_SRC_DIR': '',
+};
 
 try {
     const loginThread = await Bun.build({
         entrypoints: ['src/lostcity/server/LoginThread.ts'],
         external: modules,
+        define: defines,
         // sourcemap: 'inline',
         // minify: true,
     });
@@ -14,6 +46,7 @@ try {
     const worker = await Bun.build({
         entrypoints: ['./src/lostcity/worker.ts'],
         external: modules,
+        define: defines,
         // sourcemap: 'inline',
         // minify: true,
     });
