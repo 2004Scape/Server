5da48b3d99061a814b0eb96f276862fd6414fe62
Pazaz <pazaz@itspazaz.com>
2024-08-04 14:40:36 -0400

    feat(pack): Multiway packing into worldmap.jag

--- src/lostcity/cache/packmap/Worldmap.ts
+++ src/lostcity/cache/packmap/Worldmap.ts
@@ -9,6 +9,7 @@ import { convertImage } from '#lostcity/util/PixPack.js';
 import { LocShape } from '@2004scape/rsmod-pathfinder';
 import { shouldBuildFile, shouldBuildFileAny } from '#lostcity/util/PackFile.js';
 import NpcType from '#lostcity/cache/config/NpcType.js';
+import { Position } from '#lostcity/entity/Position.js';
 
 function packWater(underlay: Packet, overlay: Packet, mx: number, mz: number) {
     underlay.p1(mx);
@@ -52,12 +53,55 @@ export async function packWorldmap() {
     const loc = Packet.alloc(5);
     const obj = Packet.alloc(5);
     const npc = Packet.alloc(5);
+    const multi = Packet.alloc(5);
 
     function unpackCoord(packed: number): { level: number; x: number; z: number } {
         const z: number = packed & 0x3f;
         const x: number = (packed >> 6) & 0x3f;
         const level: number = (packed >> 12) & 0x3;
         return { x, z, level };
+    }
+ 
+    // easiest solution for the time being
+    const multiway = fs.readFileSync('data/src/maps/multiway.csv', 'ascii').replace(/\r/g, '').split('\n');
+    const multimap = new Set<number>();
+    for (let i = 0; i < multiway.length; i++) {
+        if (multiway[i].startsWith('//') || !multiway[i].length) {
+            continue;
+        } 
+
+        const parts = multiway[i].split(',');
+        if (parts.length === 2) {
+            const [from, to] = parts;
+            const [fromLevel, fromMx, fromMz, fromLx, fromLz] = from.split('_').map(x => parseInt(x));
+            const [toLevel, toMx, toMz, toLx, toLz] = to.split('_').map(x => parseInt(x));
+
+            if (fromLx % 8 !== 0 || fromLz % 8 !== 0 || toLx % 8 !== 7 || toLz % 8 !== 7 || fromMx > toMx || fromMz > toMz || (fromMx <= toMx && fromMz <= toMz && (fromLx > toLx || fromLz > toLz))) {
+                console.warn('Multiway map not aligned to a zone', multiway[i]);
+            }
+
+            const startX = (fromMx << 6) + fromLx;
+            const startZ = (fromMz << 6) + fromLz;
+            const endX = (toMx << 6) + toLx;
+            const endZ = (toMz << 6) + toLz;
+
+            for (let x = startX; x <= endX; x++) {
+                for (let z = startZ; z <= endZ; z++) {
+                    if (multimap.has(Position.packCoord(fromLevel, x, z))) {
+                        console.warn('Overlapping multiway map', multiway[i]);
+                    }
+                    multimap.add(Position.packCoord(fromLevel, x, z));
+                }
+            }
+        } else {
+            const [level, mx, mz, lx, lz] = multiway[i].split('_').map(x => parseInt(x));
+
+            for (let i = 0; i < 8; i++) {
+                for (let j = 0; j < 8; j++) {
+                    multimap.add(Position.packCoord(level, (mx << 6) + lx + i, (mz << 6) + lz + j));
+                }
+            }
+        }
     }
 
     const maps: string[] = fs.readdirSync('data/pack/server/maps').filter((x: string): boolean => x[0] === 'm');
@@ -381,6 +425,38 @@ export async function packWorldmap() {
                 }
             }
         }
+
+        // ---
+
+        const multis: boolean[][][] = [];
+        let hasMulti = false;
+        for (let level = 0; level < 4; level++) {
+            multis[level] = [];
+
+            for (let x = 0; x < 64; x++) {
+                multis[level][x] = [];
+
+                for (let z = 0; z < 64; z++) {
+                    multis[level][x][z] = false;
+
+                    if (multimap.has(Position.packCoord(level, (mx << 6) + x, (mz << 6) + z))) {
+                        multis[level][x][z] = true;
+                        hasMulti = true;
+                    }
+                }
+            }
+        }
+
+        if (hasMulti) {
+            multi.p1(mx);
+            multi.p1(mz);
+
+            for (let x = 0; x < 64; x++) {
+                for (let z = 0; z < 64; z++) {
+                    multi.pbool(multis[0][x][z]);
+                }
+            }
+        }
     }
 
     packWater(underlay, overlay, 39, 56);
@@ -409,6 +485,8 @@ export async function packWorldmap() {
     jag.write('obj.dat', obj);
 
     jag.write('npc.dat', npc);
+
+    jag.write('multi.dat', multi);
 
     const floorcol = Packet.alloc(1);
     floorcol.p2(FloType.configs.length);
