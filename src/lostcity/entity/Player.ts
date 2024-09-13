a5b1f0889eb43179ac8ae7105d7df698dee3bec0
tannerdino <tannerdino@gmail.com>
2024-09-12 23:44:02 -0400

    fix(engine): if no target and finished with path, clearWaypoints

--- src/lostcity/entity/Player.ts
+++ src/lostcity/entity/Player.ts
@@ -852,6 +852,10 @@ export default class Player extends PathingEntity {
     processInteraction() {
         if (this.target === null || !this.canAccess()) {
             this.updateMovement(false);
+            const {x, z} = CoordGrid.unpackCoord(this.waypoints[this.waypointIndex]);
+            if (this.isLastOrNoWaypoint() && this.x === x && this.z === z) {
+                this.clearWaypoints();
+            }
             return;
         }
 
