2d2cb7256b9e7eb14d71ff676c18fd098eb8874a
tannerdino <61213166+tannerdino@users.noreply.github.com>
2024-09-17 00:52:18 -0400

    fix(engine): revert npc_arrivedelay to old timings (#883)
    * fix(engine): revert back to previous npc_arrivedelay timings
    
    * fix(engine): update comment in npc_arrivedelay
    
    ---------
    
    Co-authored-by: tannerdino <tannerdino@gmail.com>

--- src/lostcity/engine/script/handlers/NpcOps.ts
+++ src/lostcity/engine/script/handlers/NpcOps.ts
@@ -480,11 +480,11 @@ const NpcOps: CommandHandlers = {
 
     // https://x.com/JagexAsh/status/1432296606376906752
     [ScriptOpcode.NPC_ARRIVEDELAY]: checkedHandler(ActiveNpc, state => {
-        if (state.activeNpc.lastMovement < World.currentTick - 2) {
+        if (state.activeNpc.lastMovement < World.currentTick - 1) {
             return;
         }
-        // If npc moved 2 ticks ago, delay for 1 tick. If npc moved 1 tick ago or current tick, delay for 2 ticks
-        if (state.activeNpc.lastMovement === World.currentTick - 2) {
+        // If npc moved 1 tick ago, delay for 1 tick. If npc moved this tick, delay for 2 ticks
+        if (state.activeNpc.lastMovement === World.currentTick - 1) {
             state.activeNpc.delay = 1;
         } else {
             state.activeNpc.delay = 2;
