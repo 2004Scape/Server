c619051f12d40c6183d1c3e1485dc948787d4448
tannerdino <61213166+tannerdino@users.noreply.github.com>
2024-09-17 16:03:30 -0400

    fix(engine): check for engine queue in updateMovement (#884)
    Co-authored-by: tannerdino <tannerdino@gmail.com>

--- src/lostcity/entity/Player.ts
+++ src/lostcity/entity/Player.ts
@@ -532,7 +532,7 @@ export default class Player extends PathingEntity {
 
     updateMovement(repathAllowed: boolean = true): boolean {
         // players cannot walk if they have a modal open *and* something in their queue, confirmed as far back as 2005
-        if (this.busy() && this.queue.head() != null) {
+        if (this.busy() && (this.queue.head() != null || this.engineQueue.head() != null)) {
             this.recoverEnergy(false);
             return false;
         }
