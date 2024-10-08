c0406cd41b7936ba1433d3e99b585926e6974576
tannerdino <61213166+tannerdino@users.noreply.github.com>
2024-10-08 13:35:53 -0400

    fix(engine): last_coord command to use last changed coord instead of last step (#921)
    * fix(engine): check last changed coord for last_coord instead of last step
    
    * fix(content): check if last_coord was south of ^wilderness_warning_z before showing wildy warning
    
    ---------
    
    Co-authored-by: tannerdino <tannerdino@gmail.com>

--- src/lostcity/entity/Player.ts
+++ src/lostcity/entity/Player.ts
@@ -442,6 +442,9 @@ export default class Player extends PathingEntity {
 
         this.lastStepX = this.x - 1;
         this.lastStepZ = this.z;
+        this.lastCoordX = this.x;
+        this.lastCoordZ = this.z;
+        this.lastCoordLevel = this.level;
     }
 
     triggerMapzone(x: number, z: number) {
