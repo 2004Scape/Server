3344f226f78770ce4320fe2d941f6d0621ca4d3d
Pazaz <pazaz@itspazaz.com>
2024-10-16 15:29:25 -0400

    feat: Updated compiler
    New triggers:
    - ai_spawn and changestat (todo engine implementation)
    
    Stricter syntax rules:
    - spaces are required between switch-case conditions and the separator colon
    - empty parenthesis are required for procs that return a value and accept no arguments
    - multi-line strings are not allowed
    
    Other changes:
    - null strings in scripts are packed as a string-literal "null"

--- src/lostcity/engine/script/ScriptProvider.ts
+++ src/lostcity/engine/script/ScriptProvider.ts
@@ -10,7 +10,7 @@ export default class ScriptProvider {
     /**
      * The expected version of the script compiler that the runtime should be loading scripts from.
      */
-    public static readonly COMPILER_VERSION = 19;
+    public static readonly COMPILER_VERSION = 20;
 
     /**
      * Array of loaded scripts.
