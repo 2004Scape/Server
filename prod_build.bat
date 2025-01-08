60938ec1adcfbb102c3889bd6577135eba1645fb
Rohanlogs <rohanlogs@gmail.com>
2025-01-08 16:41:27 +0200

    feat(infra): Create windows .bat script to launch production container

---
+++ prod_build.bat
@@ -0,0 +1,21 @@
+@echo off
+echo.
+
+echo [INFO] Please wait.. This can take a minute..
+docker-compose --profile fullstack up -d 
+if %ERRORLEVEL% NEQ 0 (
+    echo [!] Failed to start containers.. Something went wrong.. Stopping..
+    docker-compose down
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+echo [INFO] Services started successfully!
+
+docker-compose rm -f db_migrate
+
+echo.
+
+echo [INFO] Done!
+
+pause
