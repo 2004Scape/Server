d6617d9eb2bf4f562efabe6cee604a71c39ac199
Rohanlogs <rohanlogs@gmail.com>
2025-01-10 17:39:16 +0200

    feat(infra): Add world script & dynamic configuration

---
+++ src/prodWorldsConf.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        id: 10,
+        region: process.env.REGION || 'Docker Test #1',
+        address: `http://localhost:${process.env.WEB_PORT || 3000}`,
+        members: process.env.MEMBERS_WORLD === 'true',
+        portOffset: 0
+    },
+    {
+        id: 11,
+        region: process.env.REGION || 'Another Region #2',
+        address: `http://localhost:${process.env.WEB_PORT || 3000}`,
+        members: process.env.MEMBERS_WORLD === 'true',
+        portOffset: 0
+    }
+];
