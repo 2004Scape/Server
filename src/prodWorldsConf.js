3ceca0afa218a37e8b27dc5d6c73574542b65f3c
Rohanlogs <rohanlogs@gmail.com>
2025-01-10 18:12:08 +0200

    feat(infra): Add worlds conf, phpMyAdmin service for MySQL interface

--- src/prodWorldsConf.js
+++ src/prodWorldsConf.js
@@ -2,14 +2,14 @@ export default [
     {
         id: 10,
         region: process.env.REGION || 'Docker Test #1',
-        address: `http://localhost:${process.env.WEB_PORT || 3000}`,
+        address: `http://localhost:${process.env.WORLD_REDIRECT || 80}`,
         members: process.env.MEMBERS_WORLD === 'true',
         portOffset: 0
     },
     {
         id: 11,
         region: process.env.REGION || 'Another Region #2',
-        address: `http://localhost:${process.env.WEB_PORT || 3000}`,
+        address: `http://localhost:${process.env.WORLD_REDIRECT || 80}`,
         members: process.env.MEMBERS_WORLD === 'true',
         portOffset: 0
     }
