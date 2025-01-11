7eb53dcd22d38034826838abdbd65039fa297dbb
Jordan <personal.jordanabraham@gmail.com>
2025-01-11 12:48:50 -0500

    fix(engine): stop relying on player zones to compute shared enclosed zone buffers (#1084)

--- src/network/server/codec/ZoneMessageEncoder.ts
+++ src/network/server/codec/ZoneMessageEncoder.ts
@@ -6,10 +6,8 @@ import MessageEncoder from '#/network/server/codec/MessageEncoder.js';
 export default abstract class ZoneMessageEncoder<T extends ZoneMessage> extends MessageEncoder<T> {
     abstract prot: ZoneProt;
 
-    enclose(message: T): Uint8Array {
-        const buf: Packet = new Packet(new Uint8Array(1 + this.prot.length));
+    enclose(buf: Packet, message: T): void {
         buf.p1(this.prot.id);
         this.encode(buf, message);
-        return buf.data;
     }
 }
