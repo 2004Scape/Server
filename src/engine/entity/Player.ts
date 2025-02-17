23b8547834dd5524b5e8c4553d1b574105e11690
tannerdino <tannerdino@gmail.com>
2025-02-17 13:39:32 -0500

    revert(engine): remove unlinkQueuedScript from levelup engine queue

--- src/engine/entity/Player.ts
+++ src/engine/entity/Player.ts
@@ -1620,7 +1620,6 @@ export default class Player extends PathingEntity {
 
             const script = ScriptProvider.getByTriggerSpecific(ServerTriggerType.ADVANCESTAT, stat, -1);
             if (script) {
-                this.unlinkQueuedScript(script.id, PlayerQueueType.ENGINE);
                 this.enqueueScript(script, PlayerQueueType.ENGINE);
             }
         }
