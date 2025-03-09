20e79c9cec9c00e3c37039bb9f8727dd3eabaf4f
Mark Brandly <mebrandly@gmail.com>
2025-03-09 18:27:04 -0400

    fix(engine): Speed up Loc and Obj events by 1 tick

--- src/engine/entity/NonPathingEntity.ts
+++ src/engine/entity/NonPathingEntity.ts
@@ -4,12 +4,4 @@ export default abstract class NonPathingEntity extends Entity {
     resetEntity(_respawn: boolean) {
         // nothing happens here
     }
-
-    setLifeCycle(tick: number): void {
-        // In OSRS I suspect they use a counter per Loc/Obj to keep track of events rather than scheduling for a tick
-        // In 2004scape, we schedule for a tick. Scheduling for a tick ends up naturally 1 tick slower, so we do a -1 to compensate to match OSRS behavior
-        // - Bea5
-        this.lifecycleTick = tick - 1;
-        this.lastLifecycleTick = World.currentTick;
-    }
 }
