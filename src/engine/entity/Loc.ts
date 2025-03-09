99927df589a70baee29ff5d06b62124bd290c829
Mark Brandly <mebrandly@gmail.com>
2025-03-08 22:11:43 -0500

    fix(engine): Add change function to Loc

--- src/engine/entity/Loc.ts
+++ src/engine/entity/Loc.ts
@@ -3,23 +3,40 @@ import NonPathingEntity from '#/engine/entity/NonPathingEntity.js';
 
 export default class Loc extends NonPathingEntity {
     // constructor properties
-    private readonly info: number;
+    private info: number;
+    private tempinfo: number;
 
     constructor(level: number, x: number, z: number, width: number, length: number, lifecycle: EntityLifeCycle, type: number, shape: number, angle: number) {
         super(level, x, z, width, length, lifecycle);
         // 16383, 31, 3
         this.info = (type & 0x3fff) | ((shape & 0x1f) << 14) | ((angle & 0x3) << 19);
+        this.tempinfo = -1;
+    }
+
+    private get currentinfo() {
+        if (this.tempinfo !== -1) {
+            return this.tempinfo;
+        }
+        return this.info;
     }
 
     get type(): number {
-        return this.info & 0x3fff;
+        return this.currentinfo & 0x3fff;
     }
 
     get shape(): number {
-        return (this.info >> 14) & 0x1f;
+        return (this.currentinfo >> 14) & 0x1f;
     }
 
     get angle(): number {
-        return (this.info >> 19) & 0x3;
+        return (this.currentinfo >> 19) & 0x3;
+    }
+
+    change(type: number, shape: number, angle: number) {
+        if (this.lifecycle === EntityLifeCycle.RESPAWN) {
+            this.tempinfo = (type & 0x3fff) | ((shape & 0x1f) << 14) | ((angle & 0x3) << 19);
+        } else if (this.lifecycle === EntityLifeCycle.DESPAWN) {
+            this.info = (type & 0x3fff) | ((shape & 0x1f) << 14) | ((angle & 0x3) << 19);
+        }
     }
 }
