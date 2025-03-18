27f49824da2f6b3b8d8879f6092c062c1edde70f
Mark <mebrandly@gmail.com>
2025-03-18 14:34:07 -0400

    fix(engine): Sped up Loc and Obj events by 1 tick to match OSRS, this will make resources respawn slightly faster (#1472)
    * fix(engine): Speed up Loc and Obj events 1 tick
    
    * fix(content): Modify Al Kharid gate loc dur
    
    * fix(content): Modify West Ardougne gate to match new engine behavior
    
    * fix(content): Updated manhole, outpost gate, edgeville dungeon door speeds
    
    * fix(content): Updated gnome stronghold gate duration
    
    * fix(content): Updated yohnus, champions guild, east gate, white wolf mountain loc durs
    
    * fix(content): Updated Lava maze, doors, and double doors duration to match engine
    
    * fix(content): Updated a whole bunch of loc_adds to match engine
    
    * fix(content): Last of loc_adds updated to match engine
    
    * fix(content): Updated first half of loc_changes to match engine
    
    * fix(content): Updated second half of loc_changes to match engine
    
    * fix(content): Last few loc_dels updated
    
    * fix(content): Increased Loc events from 4m51s to 5m00s
    
    * fix(content): Change tree respawn rates to match new engine behavior
    
    ---------
    
    Co-authored-by: markb5 <mask>

--- src/engine/World.ts
+++ src/engine/World.ts
@@ -1315,8 +1315,8 @@ class World {
         // In OSRS I suspect they use a counter per Loc/Obj to keep track of events rather than scheduling for a tick
         // In 2004scape, we schedule for a tick. Scheduling for a tick ends up naturally 1 tick slower, so we do a -1 to compensate to match OSRS behavior
         // - Bea5
-        entity.setLifeCycle(this.currentTick + duration);
-        this.trackZone(this.currentTick + duration, zone);
+        entity.setLifeCycle(this.currentTick + duration - 1);
+        this.trackZone(this.currentTick + duration - 1, zone);
         this.trackZone(this.currentTick, zone);
     }
 
