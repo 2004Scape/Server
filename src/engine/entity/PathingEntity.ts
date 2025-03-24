b46f81c887365e71bdd8103ca772cc3663db1108
Jordan <personal.jordanabraham@gmail.com>
2025-03-23 20:15:31 -0400

    feat: prevent teleporting to invalid areas of the map

--- src/engine/entity/PathingEntity.ts
+++ src/engine/entity/PathingEntity.ts
@@ -268,7 +268,7 @@ export default abstract class PathingEntity extends Entity {
         }
         level = Math.max(0, Math.min(level, 3));
 
-        if (!isZoneAllocated(x, z, level)) {
+        if (!isZoneAllocated(level, x, z)) {
             if (this instanceof Player) {
                 this.messageGame('Invalid teleport!');
             }
