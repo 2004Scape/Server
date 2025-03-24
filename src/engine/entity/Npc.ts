0a5cd2027b3fd2f7587d5a0d64cb3f78cba44cb9
Pazaz <pazaz@itspazaz.com>
2025-03-24 04:42:27 -0400

    chore(engine): Minor issues from merge

--- src/engine/entity/Npc.ts
+++ src/engine/entity/Npc.ts
@@ -62,7 +62,6 @@ export default class Npc extends PathingEntity {
     huntTarget: Entity | null = null;
     huntrange: number = 0;
     spawnTriggerPending: boolean = true;
-    observerCount: number = 0;
 
     nextPatrolTick: number = -1;
     nextPatrolPoint: number = 0;
