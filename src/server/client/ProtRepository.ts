d8d6a8180552bcf49fd6096a888c8851f7aaf63e
Jordan <personal.jordanabraham@gmail.com>
2025-04-01 19:06:56 -0400

    feat: add version env property

--- src/server/client/ProtRepository.ts
+++ src/server/client/ProtRepository.ts
@@ -39,6 +39,7 @@ import ResumePauseButtonHandler from '#/server/client/handler/ResumePauseButtonH
 import ResumePCountDialogHandler from '#/server/client/handler/ResumePCountDialogHandler.js';
 import TutorialClickSideHandler from '#/server/client/handler/TutorialClickSideHandler.js';
 import MessageHandler from '#/server/client/MessageHandler.js';
+import Environment from '#/util/Environment.js';
 
 class ProtRepository {
     private readonly handlers: Map<number, MessageHandler<unknown>> = new Map();
@@ -51,103 +52,105 @@ class ProtRepository {
         this.binds.set(prot, callback);
     }
 
-    constructor() {
-        this.bind(ClientProt.CLIENT_CHEAT, (b) => rsbuf.clientCheat(b), new ClientCheatHandler());
-        this.bind(ClientProt.CLOSE_MODAL, (b) => rsbuf.closeModal(b), new CloseModalHandler());
-        this.bind(ClientProt.FRIENDLIST_ADD, (b) => rsbuf.friendListAdd(b), new FriendListAddHandler());
-        this.bind(ClientProt.FRIENDLIST_DEL, (b) => rsbuf.friendListDel(b), new FriendListDelHandler());
-        this.bind(ClientProt.IDLE_TIMER, (b) => rsbuf.idleTimer(b), new IdleTimerHandler());
-        this.bind(ClientProt.IF_BUTTON, (b) => rsbuf.ifButton(b), new IfButtonHandler());
-        this.bind(ClientProt.IF_PLAYERDESIGN, (b) => rsbuf.ifPlayerDesign(b), new IfPlayerDesignHandler());
-        this.bind(ClientProt.IGNORELIST_ADD, (b) => rsbuf.ignoreListAdd(b), new IgnoreListAddHandler());
-        this.bind(ClientProt.IGNORELIST_DEL, (b) => rsbuf.ignoreListDel(b), new IgnoreListDelHandler());
+    constructor(version: number) {
+        if (version >= 225) {
+            this.bind(ClientProt.CLIENT_CHEAT, (b) => rsbuf.clientCheat(b), new ClientCheatHandler());
+            this.bind(ClientProt.CLOSE_MODAL, (b) => rsbuf.closeModal(b), new CloseModalHandler());
+            this.bind(ClientProt.FRIENDLIST_ADD, (b) => rsbuf.friendListAdd(b), new FriendListAddHandler());
+            this.bind(ClientProt.FRIENDLIST_DEL, (b) => rsbuf.friendListDel(b), new FriendListDelHandler());
+            this.bind(ClientProt.IDLE_TIMER, (b) => rsbuf.idleTimer(b), new IdleTimerHandler());
+            this.bind(ClientProt.IF_BUTTON, (b) => rsbuf.ifButton(b), new IfButtonHandler());
+            this.bind(ClientProt.IF_PLAYERDESIGN, (b) => rsbuf.ifPlayerDesign(b), new IfPlayerDesignHandler());
+            this.bind(ClientProt.IGNORELIST_ADD, (b) => rsbuf.ignoreListAdd(b), new IgnoreListAddHandler());
+            this.bind(ClientProt.IGNORELIST_DEL, (b) => rsbuf.ignoreListDel(b), new IgnoreListDelHandler());
 
-        const invButtonHandler = new InvButtonHandler();
-        this.bind(ClientProt.INV_BUTTON1, (b) => rsbuf.invButton1(b), invButtonHandler);
-        this.bind(ClientProt.INV_BUTTON2, (b) => rsbuf.invButton2(b), invButtonHandler);
-        this.bind(ClientProt.INV_BUTTON3, (b) => rsbuf.invButton3(b), invButtonHandler);
-        this.bind(ClientProt.INV_BUTTON4, (b) => rsbuf.invButton4(b), invButtonHandler);
-        this.bind(ClientProt.INV_BUTTON5, (b) => rsbuf.invButton5(b), invButtonHandler);
-        this.bind(ClientProt.INV_BUTTOND, (b) => rsbuf.invButtonD(b), new InvButtonDHandler());
+            const invButtonHandler = new InvButtonHandler();
+            this.bind(ClientProt.INV_BUTTON1, (b) => rsbuf.invButton1(b), invButtonHandler);
+            this.bind(ClientProt.INV_BUTTON2, (b) => rsbuf.invButton2(b), invButtonHandler);
+            this.bind(ClientProt.INV_BUTTON3, (b) => rsbuf.invButton3(b), invButtonHandler);
+            this.bind(ClientProt.INV_BUTTON4, (b) => rsbuf.invButton4(b), invButtonHandler);
+            this.bind(ClientProt.INV_BUTTON5, (b) => rsbuf.invButton5(b), invButtonHandler);
+            this.bind(ClientProt.INV_BUTTOND, (b) => rsbuf.invButtonD(b), new InvButtonDHandler());
 
-        this.bind(ClientProt.MESSAGE_PRIVATE, (b) => rsbuf.messagePrivate(b), new MessagePrivateHandler());
-        this.bind(ClientProt.MESSAGE_PUBLIC, (b) => rsbuf.messagePublic(b), new MessagePublicHandler());
+            this.bind(ClientProt.MESSAGE_PRIVATE, (b) => rsbuf.messagePrivate(b), new MessagePrivateHandler());
+            this.bind(ClientProt.MESSAGE_PUBLIC, (b) => rsbuf.messagePublic(b), new MessagePublicHandler());
 
-        const moveClickHandler = new MoveClickHandler();
-        this.bind(ClientProt.MOVE_GAMECLICK, (b) => rsbuf.moveGameClick(b), moveClickHandler);
-        this.bind(ClientProt.MOVE_OPCLICK, (b) => rsbuf.moveGameClick(b), moveClickHandler);
-        this.bind(ClientProt.MOVE_MINIMAPCLICK, (b) => rsbuf.moveMinimapClick(b), moveClickHandler);
+            const moveClickHandler = new MoveClickHandler();
+            this.bind(ClientProt.MOVE_GAMECLICK, (b) => rsbuf.moveGameClick(b), moveClickHandler);
+            this.bind(ClientProt.MOVE_OPCLICK, (b) => rsbuf.moveGameClick(b), moveClickHandler);
+            this.bind(ClientProt.MOVE_MINIMAPCLICK, (b) => rsbuf.moveMinimapClick(b), moveClickHandler);
 
-        this.bind(ClientProt.NO_TIMEOUT, (b) => rsbuf.noTimeout(b), new NoTimeoutHandler());
+            this.bind(ClientProt.NO_TIMEOUT, (b) => rsbuf.noTimeout(b), new NoTimeoutHandler());
 
-        const opHeldHandler = new OpHeldHandler();
-        this.bind(ClientProt.OPHELD1, (b) => rsbuf.opheld1(b), opHeldHandler);
-        this.bind(ClientProt.OPHELD2, (b) => rsbuf.opheld2(b), opHeldHandler);
-        this.bind(ClientProt.OPHELD3, (b) => rsbuf.opheld3(b), opHeldHandler);
-        this.bind(ClientProt.OPHELD4, (b) => rsbuf.opheld4(b), opHeldHandler);
-        this.bind(ClientProt.OPHELD5, (b) => rsbuf.opheld5(b), opHeldHandler);
-        this.bind(ClientProt.OPHELDT, (b) => rsbuf.opheldT(b), new OpHeldTHandler());
-        this.bind(ClientProt.OPHELDU, (b) => rsbuf.opheldU(b), new OpHeldUHandler());
+            const opHeldHandler = new OpHeldHandler();
+            this.bind(ClientProt.OPHELD1, (b) => rsbuf.opheld1(b), opHeldHandler);
+            this.bind(ClientProt.OPHELD2, (b) => rsbuf.opheld2(b), opHeldHandler);
+            this.bind(ClientProt.OPHELD3, (b) => rsbuf.opheld3(b), opHeldHandler);
+            this.bind(ClientProt.OPHELD4, (b) => rsbuf.opheld4(b), opHeldHandler);
+            this.bind(ClientProt.OPHELD5, (b) => rsbuf.opheld5(b), opHeldHandler);
+            this.bind(ClientProt.OPHELDT, (b) => rsbuf.opheldT(b), new OpHeldTHandler());
+            this.bind(ClientProt.OPHELDU, (b) => rsbuf.opheldU(b), new OpHeldUHandler());
 
-        const opLocHandler = new OpLocHandler();
-        this.bind(ClientProt.OPLOC1, (b) => rsbuf.oploc1(b), opLocHandler);
-        this.bind(ClientProt.OPLOC2, (b) => rsbuf.oploc2(b), opLocHandler);
-        this.bind(ClientProt.OPLOC3, (b) => rsbuf.oploc3(b), opLocHandler);
-        this.bind(ClientProt.OPLOC4, (b) => rsbuf.oploc4(b), opLocHandler);
-        this.bind(ClientProt.OPLOC5, (b) => rsbuf.oploc5(b), opLocHandler);
-        this.bind(ClientProt.OPLOCT, (b) => rsbuf.oplocT(b), new OpLocTHandler());
-        this.bind(ClientProt.OPLOCU, (b) => rsbuf.oplocU(b), new OpLocUHandler());
+            const opLocHandler = new OpLocHandler();
+            this.bind(ClientProt.OPLOC1, (b) => rsbuf.oploc1(b), opLocHandler);
+            this.bind(ClientProt.OPLOC2, (b) => rsbuf.oploc2(b), opLocHandler);
+            this.bind(ClientProt.OPLOC3, (b) => rsbuf.oploc3(b), opLocHandler);
+            this.bind(ClientProt.OPLOC4, (b) => rsbuf.oploc4(b), opLocHandler);
+            this.bind(ClientProt.OPLOC5, (b) => rsbuf.oploc5(b), opLocHandler);
+            this.bind(ClientProt.OPLOCT, (b) => rsbuf.oplocT(b), new OpLocTHandler());
+            this.bind(ClientProt.OPLOCU, (b) => rsbuf.oplocU(b), new OpLocUHandler());
 
-        const opNpcHandler = new OpNpcHandler();
-        this.bind(ClientProt.OPNPC1, (b) => rsbuf.opnpc1(b), opNpcHandler);
-        this.bind(ClientProt.OPNPC2, (b) => rsbuf.opnpc2(b), opNpcHandler);
-        this.bind(ClientProt.OPNPC3, (b) => rsbuf.opnpc3(b), opNpcHandler);
-        this.bind(ClientProt.OPNPC4, (b) => rsbuf.opnpc4(b), opNpcHandler);
-        this.bind(ClientProt.OPNPC5, (b) => rsbuf.opnpc5(b), opNpcHandler);
-        this.bind(ClientProt.OPNPCT, (b) => rsbuf.opnpcT(b), new OpNpcTHandler());
-        this.bind(ClientProt.OPNPCU, (b) => rsbuf.opnpcU(b), new OpNpcUHandler());
+            const opNpcHandler = new OpNpcHandler();
+            this.bind(ClientProt.OPNPC1, (b) => rsbuf.opnpc1(b), opNpcHandler);
+            this.bind(ClientProt.OPNPC2, (b) => rsbuf.opnpc2(b), opNpcHandler);
+            this.bind(ClientProt.OPNPC3, (b) => rsbuf.opnpc3(b), opNpcHandler);
+            this.bind(ClientProt.OPNPC4, (b) => rsbuf.opnpc4(b), opNpcHandler);
+            this.bind(ClientProt.OPNPC5, (b) => rsbuf.opnpc5(b), opNpcHandler);
+            this.bind(ClientProt.OPNPCT, (b) => rsbuf.opnpcT(b), new OpNpcTHandler());
+            this.bind(ClientProt.OPNPCU, (b) => rsbuf.opnpcU(b), new OpNpcUHandler());
 
-        const opObjHandler = new OpObjHandler();
-        this.bind(ClientProt.OPOBJ1, (b) => rsbuf.opobj1(b), opObjHandler);
-        this.bind(ClientProt.OPOBJ2, (b) => rsbuf.opobj2(b), opObjHandler);
-        this.bind(ClientProt.OPOBJ3, (b) => rsbuf.opobj3(b), opObjHandler);
-        this.bind(ClientProt.OPOBJ4, (b) => rsbuf.opobj4(b), opObjHandler);
-        this.bind(ClientProt.OPOBJ5, (b) => rsbuf.opobj5(b), opObjHandler);
-        this.bind(ClientProt.OPOBJT, (b) => rsbuf.opobjT(b), new OpObjTHandler());
-        this.bind(ClientProt.OPOBJU, (b) => rsbuf.opobjU(b), new OpObjUHandler());
+            const opObjHandler = new OpObjHandler();
+            this.bind(ClientProt.OPOBJ1, (b) => rsbuf.opobj1(b), opObjHandler);
+            this.bind(ClientProt.OPOBJ2, (b) => rsbuf.opobj2(b), opObjHandler);
+            this.bind(ClientProt.OPOBJ3, (b) => rsbuf.opobj3(b), opObjHandler);
+            this.bind(ClientProt.OPOBJ4, (b) => rsbuf.opobj4(b), opObjHandler);
+            this.bind(ClientProt.OPOBJ5, (b) => rsbuf.opobj5(b), opObjHandler);
+            this.bind(ClientProt.OPOBJT, (b) => rsbuf.opobjT(b), new OpObjTHandler());
+            this.bind(ClientProt.OPOBJU, (b) => rsbuf.opobjU(b), new OpObjUHandler());
 
-        const opPlayerHandler = new OpPlayerHandler();
-        this.bind(ClientProt.OPPLAYER1, (b) => rsbuf.opplayer1(b), opPlayerHandler);
-        this.bind(ClientProt.OPPLAYER2, (b) => rsbuf.opplayer2(b), opPlayerHandler);
-        this.bind(ClientProt.OPPLAYER3, (b) => rsbuf.opplayer3(b), opPlayerHandler);
-        this.bind(ClientProt.OPPLAYER4, (b) => rsbuf.opplayer4(b), opPlayerHandler);
-        this.bind(ClientProt.OPPLAYERT, (b) => rsbuf.opplayerT(b), new OpPlayerTHandler());
-        this.bind(ClientProt.OPPLAYERU, (b) => rsbuf.opplayerU(b), new OpPlayerUHandler());
+            const opPlayerHandler = new OpPlayerHandler();
+            this.bind(ClientProt.OPPLAYER1, (b) => rsbuf.opplayer1(b), opPlayerHandler);
+            this.bind(ClientProt.OPPLAYER2, (b) => rsbuf.opplayer2(b), opPlayerHandler);
+            this.bind(ClientProt.OPPLAYER3, (b) => rsbuf.opplayer3(b), opPlayerHandler);
+            this.bind(ClientProt.OPPLAYER4, (b) => rsbuf.opplayer4(b), opPlayerHandler);
+            this.bind(ClientProt.OPPLAYERT, (b) => rsbuf.opplayerT(b), new OpPlayerTHandler());
+            this.bind(ClientProt.OPPLAYERU, (b) => rsbuf.opplayerU(b), new OpPlayerUHandler());
 
-        this.bind(ClientProt.REBUILD_GETMAPS, (b) => rsbuf.rebuildGetMaps(b), new RebuildGetMapsHandler());
-        this.bind(ClientProt.RESUME_PAUSEBUTTON, (b) => rsbuf.resumePauseButton(b), new ResumePauseButtonHandler());
-        this.bind(ClientProt.RESUME_P_COUNTDIALOG, (b) => rsbuf.resumeCountDialog(b), new ResumePCountDialogHandler());
-        this.bind(ClientProt.TUTORIAL_CLICKSIDE, (b) => rsbuf.tutorialClickSide(b), new TutorialClickSideHandler());
-        this.bind(ClientProt.CHAT_SETMODE, (b) => rsbuf.chatSetMode(b), new ChatSetModeHandler());
-        this.bind(ClientProt.EVENT_TRACKING, (b) => rsbuf.eventTracking(b), new EventTrackingHandler());
-        this.bind(ClientProt.REPORT_ABUSE, (b) => rsbuf.reportAbuse(b), new ReportAbuseHandler());
+            this.bind(ClientProt.REBUILD_GETMAPS, (b) => rsbuf.rebuildGetMaps(b), new RebuildGetMapsHandler());
+            this.bind(ClientProt.RESUME_PAUSEBUTTON, (b) => rsbuf.resumePauseButton(b), new ResumePauseButtonHandler());
+            this.bind(ClientProt.RESUME_P_COUNTDIALOG, (b) => rsbuf.resumeCountDialog(b), new ResumePCountDialogHandler());
+            this.bind(ClientProt.TUTORIAL_CLICKSIDE, (b) => rsbuf.tutorialClickSide(b), new TutorialClickSideHandler());
+            this.bind(ClientProt.CHAT_SETMODE, (b) => rsbuf.chatSetMode(b), new ChatSetModeHandler());
+            this.bind(ClientProt.EVENT_TRACKING, (b) => rsbuf.eventTracking(b), new EventTrackingHandler());
+            this.bind(ClientProt.REPORT_ABUSE, (b) => rsbuf.reportAbuse(b), new ReportAbuseHandler());
 
-        this.bind(ClientProt.EVENT_CAMERA_POSITION, (b) => rsbuf.eventCameraPosition(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC1, (b) => rsbuf.anticheatOp1(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC2, (b) => rsbuf.anticheatOp2(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC3, (b) => rsbuf.anticheatOp3(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC4, (b) => rsbuf.anticheatOp4(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC5, (b) => rsbuf.anticheatOp5(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC6, (b) => rsbuf.anticheatOp6(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC7, (b) => rsbuf.anticheatOp7(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC8, (b) => rsbuf.anticheatOp8(b));
-        this.bind(ClientProt.ANTICHEAT_OPLOGIC9, (b) => rsbuf.anticheatOp9(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC1, (b) => rsbuf.anticheatCycle1(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC2, (b) => rsbuf.anticheatCycle2(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC3, (b) => rsbuf.anticheatCycle3(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC4, (b) => rsbuf.anticheatCycle4(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC5, (b) => rsbuf.anticheatCycle5(b));
-        this.bind(ClientProt.ANTICHEAT_CYCLELOGIC6, (b) => rsbuf.anticheatCycle6(b));
+            this.bind(ClientProt.EVENT_CAMERA_POSITION, (b) => rsbuf.eventCameraPosition(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC1, (b) => rsbuf.anticheatOp1(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC2, (b) => rsbuf.anticheatOp2(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC3, (b) => rsbuf.anticheatOp3(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC4, (b) => rsbuf.anticheatOp4(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC5, (b) => rsbuf.anticheatOp5(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC6, (b) => rsbuf.anticheatOp6(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC7, (b) => rsbuf.anticheatOp7(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC8, (b) => rsbuf.anticheatOp8(b));
+            this.bind(ClientProt.ANTICHEAT_OPLOGIC9, (b) => rsbuf.anticheatOp9(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC1, (b) => rsbuf.anticheatCycle1(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC2, (b) => rsbuf.anticheatCycle2(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC3, (b) => rsbuf.anticheatCycle3(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC4, (b) => rsbuf.anticheatCycle4(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC5, (b) => rsbuf.anticheatCycle5(b));
+            this.bind(ClientProt.ANTICHEAT_CYCLELOGIC6, (b) => rsbuf.anticheatCycle6(b));
+        }
     }
 
     getHandler(prot: ClientProt): MessageHandler<unknown> | undefined {
@@ -159,4 +162,4 @@ class ProtRepository {
     }
 }
 
-export default new ProtRepository();
+export default new ProtRepository(Environment.NODE_VERSION);
