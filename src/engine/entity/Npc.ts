83b2537fd1248ec24944941a76b90fd95fc83277
Pazaz <pazaz@itspazaz.com>
2025-04-01 14:55:12 -0400

    revert(engine): Npcs will now reset properly if they have not wandered in 500 ticks (#1596)
    This reverts commit 4cd01b0271f3c494910b4aaa3426e555a6c96456.

--- src/engine/entity/Npc.ts
+++ src/engine/entity/Npc.ts
@@ -202,7 +202,6 @@ export default class Npc extends PathingEntity {
         const moved = this.lastTickX !== this.x || this.lastTickZ !== this.z;
         if (moved) {
             this.lastMovement = World.currentTick + 1;
-            this.lastWanderTick = World.currentTick;
         }
         return moved;
     }
@@ -420,13 +419,9 @@ export default class Npc extends PathingEntity {
         if (this.delayed) {
             return;
         }
-
         if (this.targetOp === NpcMode.NULL) {
-            const type: NpcType = NpcType.get(this.type);
-            this.targetOp = type.defaultmode;
-        }
-
-        if (this.targetOp === NpcMode.NONE) {
+            this.defaultMode();
+        } else if (this.targetOp === NpcMode.NONE) {
             this.noMode();
         } else if (this.targetOp === NpcMode.WANDER) {
             this.wanderMode();
@@ -460,6 +455,7 @@ export default class Npc extends PathingEntity {
         this.clearInteraction();
         const type: NpcType = NpcType.get(this.type);
         this.targetOp = type.defaultmode;
+        this.lastWanderTick = World.currentTick; // osrs
         this.faceEntity = -1;
         this.masks |= this.entitymask;
 
@@ -473,17 +469,19 @@ export default class Npc extends PathingEntity {
 
     wanderMode(): void {
         const type = NpcType.get(this.type);
-
-        // 1/8 chance to move every tick (even if they already have a destination)
         if (type.moverestrict !== MoveRestrict.NOMOVE && Math.random() < 0.125) {
+            // 1/8 chance to move every tick (even if they already have a destination)
             this.randomWalk(type.wanderrange);
+            const moved = this.updateMovement(false);
+            if (moved) {
+                this.lastWanderTick = World.currentTick;
+            } else if (World.currentTick > this.lastWanderTick + 500) {
+                this.teleport(this.startX, this.startZ, this.startLevel);
+                this.lastWanderTick = World.currentTick;
+            }
+            return;
         }
-
         this.updateMovement(false);
-
-        if (World.currentTick > this.lastWanderTick + 500) {
-            this.teleport(this.startX, this.startZ, this.startLevel);
-        }
     }
 
     patrolMode(): void {
@@ -605,6 +603,7 @@ export default class Npc extends PathingEntity {
         }
 
         if (this.level !== this.target.level) {
+            this.clearWaypoints();
             this.defaultMode();
             return;
         }
@@ -628,6 +627,7 @@ export default class Npc extends PathingEntity {
         }
 
         if (this.level !== this.target.level) {
+            this.clearWaypoints();
             this.defaultMode();
             return;
         }
@@ -644,9 +644,6 @@ export default class Npc extends PathingEntity {
             this.defaultMode();
             return;
         }
-
-        // Reset 500 tick wander reset clock
-        this.lastWanderTick = World.currentTick;
 
         // Try to interact before moving, include op Obj and Loc
         if (this.tryInteract(true)) {
