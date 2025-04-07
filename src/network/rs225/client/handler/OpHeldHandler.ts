7e45c3a5fd68988fb1f126e0c12458bfa20e36f4
AmVoidGuy <132288247+AmVoidGuy@users.noreply.github.com>
2025-04-07 10:06:06 -0500

    fix(engine): Don't clearPendingAction on main modal ophelds (Puzzlebox fix) (#1633)
    * fix(engine): No longer call clearPendingAction on main modal ophelds. (Puzzlebox fix)
    
    * fix: Whitespace

--- src/network/rs225/client/handler/OpHeldHandler.ts
+++ src/network/rs225/client/handler/OpHeldHandler.ts
@@ -44,7 +44,10 @@ export default class OpHeldHandler extends MessageHandler<OpHeld> {
         player.lastItem = item;
         player.lastSlot = slot;
 
-        player.clearPendingAction();
+        if(com.rootLayer != player.modalMain) {
+            player.clearPendingAction();
+        }
+
         player.moveClickRequest = false; // uses the dueling ring op to move whilst busy & queue pending: https://youtu.be/GPfN3Isl2rM
         player.faceEntity = -1;
         player.masks |= player.entitymask;
